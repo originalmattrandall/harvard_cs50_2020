e0eeeecbb443a50c22012a49b5ddb599253edcbd
Matthew Randall <mattrandall01@gmail.com>
2020-05-17 13:58:11 -0400

    mario less and more

---
+++ Problem Set 6/README.md
@@ -0,0 +1,9 @@
+## mario_less
+
+create a single sided pyramid similar to the end of the mario games
+Maximum input of 8
+
+## mario_more
+
+create a double sided pyramid similar to the end of the mario games
+Maximum input of 8
---
+++ Problem Set 6/mario_more.py
@@ -0,0 +1,19 @@
+def main():
+    i = get_positive_int()
+    build_pieces(i)
+
+def get_positive_int():
+    while True:
+        try:
+            n = int(input("Height: "))
+            if n > 0 and n < 9:
+                break
+        except:
+            print("must be a non negative integer")
+    return n
+
+def build_pieces(height):
+    for brick in range(height):
+        print(("#" * (brick + 1)).rjust(height) + "  " + "#" * (brick + 1))
+
+main()
